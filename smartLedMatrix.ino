e1e4d60610f3214f86a79f4a664b0e999e3eb22f
Kwok Hung Chu <chu_kwokhung@yahoo.com.hk>
2017-09-07 22:00:36 +0800

    init

--- smartLedMatrix.ino
+++ smartLedMatrix.ino
@@ -5,11 +5,18 @@ App app;
 void setup()
 {
     app.setup();
+
+    for (int i = 0; i < LedMatrix::columnSize; i++)
+    {
+        app.getLedMatrix()->putCharToDisplayMatrix(i,
+                                                   0,
+                                                   LedMatrix::columnSize - i,
+                                                   false);
+    };
 };
 
-void loop()
-{
-    app.getLedMatrix()->loop();
+void loop(){
+    //app.getLedMatrix()->loop();
 };
 
 ISR(TIMER2_COMPA_vect)
